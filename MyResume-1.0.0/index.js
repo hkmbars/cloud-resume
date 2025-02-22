e7783494ade4308739752692a9e135a997493149
Hannah <hkmbars@gmail.com>
2025-02-22 23:26:46 +0800

    JS update comment

--- MyResume-1.0.0/index.js
+++ MyResume-1.0.0/index.js
@@ -1,9 +1,18 @@
-const counter = document.querySelector(".views-counter"); //creating a variable to select a document from index.html file
-async function updateCounter () {                         //create a function which is doing a fetch request to the URL
-    let response = await fetch("https://nda6dsqlrrgxctmfv6wugtdkkm0fykzc.lambda-url.ap-southeast-1.on.aws/"); //aws lambda function url
-    let data = await response.json();                     //similar to curl request storing the result as a variable called data
-    counter.innerHTML = `    Views: <span class="views">  ${data}  </span>`; // updates the views-counter
+//creates a variable to select an element from index.html file which is views-counter
+const counter = document.querySelector(".views-counter"); 
 
+// Defines an asynchronous function to fetch and update the counter
+async function updateCounter () {        
+
+     // Sends a request to the AWS Lambda function URL and waits for the response. The URL is the aws lambda function url
+    let response = await fetch("https://nda6dsqlrrgxctmfv6wugtdkkm0fykzc.lambda-url.ap-southeast-1.on.aws/"); 
+
+    // Converts the response into a JavaScript object and stores it in the "data" variable
+    let data = await response.json();      
+
+    // Updates the HTML content of the selected element with the retrieved view count     
+    counter.innerHTML = `    Views: <span class="views">  ${data}  </span>`; 
 }
 
+// Calls the function to execute when the script runs
 updateCounter();
